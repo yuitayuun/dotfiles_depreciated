348fb637dd8d53379284473978a017a49bd78de3
yuitayuun <wojtek@int.pl>
2025-07-21 18:21:06 +0200

    moving home so i can make it multi-machine

--- home/default.nix
+++ home/default.nix
@@ -119,3 +119,4 @@
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
--- hosts/pc/visual.nix
+++ hosts/pc/visual.nix
@@ -1,5 +1,24 @@
 { config, lib, pkgs, system, inputs, ... }:
 {
+    programs.niri.settings.outputs = {
+      "DP-1" = {
+        scale = 1.5;
+        position = {
+          x = 1800;
+          y = 300;
+        };
+      };
+      "HDMI-A-1" = {
+        scale = 1.25;
+        transform = {
+          "90"
+        }
+        position = {
+          x = 600;
+          y = 100;
+        };   
+      };   
+    };
   
 }
 
