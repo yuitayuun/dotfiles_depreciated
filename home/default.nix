9cd7793662479ebcbd73a93250203751badd7cc6
yuitayuun <wojtek@int.pl>
2025-08-08 22:44:14 +0200

    removed lingering code

--- home/default.nix
+++ home/default.nix
@@ -19,6 +19,7 @@
     jetbrains.pycharm-professional
     anki-bin
      # Tools
+    ripdrag
     alacritty
     nix-search-tv
     unrar
@@ -86,32 +87,6 @@
     ./git.nix
     ./cachix.nix
   ];
-
-
-  # services.swayidle = {
-  #   enable = true;
-  #   timeouts = [
-  #     {
-  #       timeout = 240;
-  #       command = "${pkgs.systemd}/bin/loginctl lock-session";
-  #     }
-  #     {
-  #       timeout = 420;
-  #       command = "${pkgs.systemd}/bin/systemctl sleep";
-  #     }
-  #   ];
-  #   events = [
-  #     {
-  #       event = "lock";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --indicator --clock --effect-blur 10x3 --layout-bg-color FFFFFF --ring-color 4B0076 --key-hl-color 000000 --text-color FF13F0 --inside-ver-color FF13F0 --text-ver-color 000000 --ring-ver-color 000000 &";
-  #     }
-  #     {
-  #       event = "before-sleep";
-  #       command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --indicator --clock --effect-blur 10x3 --layout-bg-color FFFFFF --ring-color 4B0076 --key-hl-color 000000 --text-color FF13F0 --inside-ver-color FF13F0 --text-ver-color 000000 --ring-ver-color 000000 &";
-  #     }
-  #   ];
-  # };
-    
 
 
   # Zen Browser
