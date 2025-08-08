26a95fea12edb710968c18819d16e563305f20c0
yuitayuun <wojtek@int.pl>
2025-08-08 23:24:49 +0200

    moving keeb config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,7 +8,7 @@
   imports = [ 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../keeb.nix
+      ./keyboard/keeb.nix
       ../../sharedconfig.nix
     ];
       # GTK Theme!
