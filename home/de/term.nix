18a62a0217a78eba07b35e4eb409ce1e685fc8a4
yuitayuun <wojtek@int.pl>
2025-08-09 21:51:32 +0200

    rewrote lua in a string

--- home/de/term.nix
+++ home/de/term.nix
@@ -10,7 +10,7 @@
   # Yazi!
   programs.yazi = {
     enable = true;
-    initLua = ./yazi/init.lua;
+
     settings = {
       mgr = {
         ratio = [
@@ -69,6 +69,30 @@
         desc = "Start bunny.yazi";
       }
     ];
+    initLua = ''
+        require("bunny"):setup({
+        hops = {
+          { key = "/",          path = "/",                                                   },
+          { key = "t",          path = "/tmp",                                                },
+          { key = "n",          path = "~/dotfiles",       desc = "doftiles!"                 },
+          { key = "~",          path = "~",                desc = "Home"                      },
+          { key = "m",          path = "~/Music",          desc = "Music"                     },
+          { key = "v",          path = "~/Videos",          desc = "Videos"                   },
+          { key = "d",          path = "~/Desktop",        desc = "Desktop"                   },
+          { key = "D",          path = "~/Documents",      desc = "Documents"                 },
+          { key = "c",          path = "~/.config",        desc = "Config files"              },
+          { key = { "l", "s" }, path = "~/.local/share",   desc = "Local share"               },
+          { key = { "l", "b" }, path = "~/.local/bin",     desc = "Local bin"                 },
+          { key = { "l", "t" }, path = "~/.local/state",   desc = "Local state"               },
+          { key = "s",          path = "~/.local/share/Steam/steamapps/common", desc = "Steam"},
+          -- key and path attributes are required, desc is optional
+        },
+        desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+        ephemeral = true, -- Enable ephemeral hops, default is true
+        tabs = true, -- Enable tab hops, default is true
+        notify = false, -- Notify after hopping, default is false
+        fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+      })'';
   };
 
   # home.file.".config/yazi/init.lua.source" = ./yaziinit.lua;
