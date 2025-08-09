7c8c8d3950ecd74203cf96c4b9f002fec447c54b
yuitayuun <wojtek@int.pl>
2025-08-09 13:50:48 +0200

    moved packages to external file

--- home/default.nix
+++ home/default.nix
@@ -3,65 +3,6 @@
   home.username = "yui";
   home.homeDirectory = "/home/yui";
   systemd.user = {inherit (config.home) sessionVariables;};
-  home.packages = with pkgs; [
-    # Communicators
-    dorion
-    equibop
-    fractal
-    chromium
-    # Looks
-    swww
-    magnetic-catppuccin-gtk
-    swaybg
-    # Work
-    obsidian
-    onlyoffice-bin_latest
-    jetbrains.pycharm-professional
-    anki-bin
-     # Tools
-    ripdrag
-    alacritty
-    nix-search-tv
-    unrar
-    isoimagewriter
-    cider
-    yt-dlp
-    flitter
-    nemo
-    brightnessctl
-    gparted
-    jq
-    fastfetch
-    wlsunset
-    bitwarden
-    hyprpicker
-    htop
-    gpu-screen-recorder
-    gpu-screen-recorder-gtk    
-    rustdesk-flutter
-    pwvucontrol
-     # Compatibility        
-    nix-alien
-    bottles
-    wine    
-    qemu
-    quickemu
-     # Gaming
-    lutris
-    gamescope            
-    ppsspp-sdl-wayland
-    xivlauncher
-    heroic
-    
-    # (zen-browser.override {
-    #   extraPrefsFiles = [
-    #     (builtins.fetchurl {  
-    #       url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
-    #       sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
-    #     })
-    #   ];
-    # })
-  ];
 
 
   programs.niri.package = inputs.niri.packages.${system}.niri-unstable;
@@ -82,6 +23,7 @@
     
   # Imports
   imports = [
+    ./packages.nix
     ./de
     ./de/term.nix
     ./git.nix
--- home/packages.nix
+++ home/packages.nix
@@ -1,7 +1,63 @@
-  { pkgs, ... }:
-  with pkgs; [
-  helix
-  kitty
-  git
-  # more packages go here
-  ]
+{ config, lib, pkgs, system, inputs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Communicators
+    dorion
+    equibop
+    fractal
+    chromium
+    # Looks
+    swww
+    magnetic-catppuccin-gtk
+    swaybg
+    # Work
+    obsidian
+    onlyoffice-bin_latest
+    jetbrains.pycharm-professional
+    anki-bin
+     # Tools
+    ripdrag
+    alacritty
+    nix-search-tv
+    unrar
+    isoimagewriter
+    cider
+    yt-dlp
+    flitter
+    nemo
+    brightnessctl
+    gparted
+    jq
+    fastfetch
+    wlsunset
+    bitwarden
+    hyprpicker
+    htop
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk    
+    rustdesk-flutter
+    pwvucontrol
+     # Compatibility        
+    nix-alien
+    bottles
+    wine    
+    qemu
+    quickemu
+     # Gaming
+    lutris
+    gamescope            
+    ppsspp-sdl-wayland
+    xivlauncher
+    heroic
+    
+    # (zen-browser.override {
+    #   extraPrefsFiles = [
+    #     (builtins.fetchurl {  
+    #       url = "https://raw.githubusercontent.com/MrOtherGuy/fx-autoconfig/master/program/config.js";
+    #       sha256 = "1mx679fbc4d9x4bnqajqx5a95y1lfasvf90pbqkh9sm3ch945p40";
+    #     })
+    #   ];
+    # })
+  ];
+}
