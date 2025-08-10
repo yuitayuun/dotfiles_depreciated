c7333bdf328ccd2b8858feca0227abeb030becd8
yuitayuun <wojtek@int.pl>
2025-08-10 21:01:58 +0200

    moved a monitor to the right

--- hosts/laptop/home/default.nix
+++ hosts/laptop/home/default.nix
@@ -11,14 +11,14 @@
     "eDP-1" = {
       scale = 1.25;
       position = {
-        x = 2561;
+        x = 0;
         y = 200;
       };
     };
     "HDMI-A-1" = {
       scale = 1.5;
       position = {
-        x = 0;
+        x = 2561;
         y = 0;
       };
     };
