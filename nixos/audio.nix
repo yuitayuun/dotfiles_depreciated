280f44e4fc1e03545265fbae254e4f0479b253ae
yuitayuun <wojtek@int.pl>
2025-08-20 02:17:35 +0200

    MUSIC OVERHAUL

---
+++ nixos/audio.nix
@@ -0,0 +1,37 @@
+{
+  # Enable sound with pipewire. ALSO MUSIC STUFFS HERE;
+  musnix.enable = true;
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 256;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 256;
+      };
+    };
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
